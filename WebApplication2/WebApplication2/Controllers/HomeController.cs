bbfc85ee7538bfd7cb4c74bcbbcbde6f23a0acfe
Vojnovič Milan <vojnovic@avast.com>
2016-03-02 12:35:56 +0100

    Automatic Output Encoding

--- WebApplication2/WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
@@ -10,7 +11,7 @@ namespace WebApplication2.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new MyModel());
         }
 
         public ActionResult About()
